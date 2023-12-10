3121e51c4dbd9a4b65ed3b8d7004a7ccb170b327
OuLour <49345524+Lourmati@users.noreply.github.com>
2023-12-09 23:10:04 -0500

    Add correct files

--- expense-tracker-app/screens/AllExpenses.js
+++ expense-tracker-app/screens/AllExpenses.js
@@ -1,7 +1,17 @@
-import { View, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import ExpensesOutput from "../components/Expenses/ExpensesOutput";
+import { useContext } from "react";
+import { ExpensesContext } from "../store/expenses-context";
 
 function AllExpenses() {
-  return <View></View>;
+  const expensesCtx = useContext(ExpensesContext);
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No expenses registered found."
+    />
+  );
 }
 
 export default AllExpenses;
